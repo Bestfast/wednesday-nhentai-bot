cbda102865cd839fffc3596d695467df5e4b3492
Bestfast <bestfast03@gmail.com>
2018-07-02 09:22:44 +0200

    Added a template using the SQLite3 db

--- bot.py
+++ bot.py
@@ -16,14 +16,45 @@ def start_command(chat, message, args):
         c.execute("SELECT ? FROM users", (message.sender.id,))
     
     except sqlite3.OperationalError:
-        c.execute("CREATE TABLE users (user_id INTEGER);")
+        c.execute("CREATE TABLE users (user_id INTEGER, lang TEXT);")
     
     data = c.fetchall()
     
     if not data:
-        c.execute('INSERT INTO users VALUES (?)', (message.sender.id,))
+
+        btns = botogram.Buttons()
+        btns[0].callback("IT", "lang_it")
+        btns[1].callback("EN", "lang_en")
+
+        chat.send("Hi! First of all, select your language!", attach=btns)
+        
         conn.commit()
     
     conn.close()
+
+
+
+@bot.callback("lang_it")
+def lang_it(query, chat, message):
+
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    try:
+        c.execute('DELETE FROM users WHERE user_id = ?', (message.sender.id,))
+    except sqlite3.OperationalError:
+        c.execute("CREATE TABLE users (user_id INTEGER, lang TEXT);")
+
+    c.execute('INSERT INTO users VALUES (?, ?)', (message.sender.id, "it",))
+
+    conn.commit()
+    conn.close()
+
+    bot.edit_message(query.sender.id,  query.message.message_id, "Done!")
+    
+
+@bot.callback("lang_en")
+def lang_en(query, chat, message):
+    print("to do")
 
 bot.run()
